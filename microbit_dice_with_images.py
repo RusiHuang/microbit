d4404d82b5fd3f6e4ff2510bfba5353ae15dad36
Rusi Huang <rusiiihuang@gmail.com>
2021-09-16 21:18:58 +1000

    add dice python source

---
+++ microbit_dice_with_images.py
@@ -0,0 +1,39 @@
+from microbit import *
+import random
+num_shakes = 0
+numbers = [Image('00000:'
+                 '00900:'
+                 '00900:'
+                 '00900:'
+                 '00900:'), 
+          Image('09900:'
+                '90090:'
+                '00900:'
+                '09000:'
+                '99990:'),
+          Image('00999:'
+                '00009:'
+                '00999:'
+                '00009:'
+                '00999:'), 
+          Image('90090:'
+                '90090:'
+                '99990:'
+                '00090:'
+                '00090:'), 
+          Image('09990:'
+                '90000:'
+                '09900:'
+                '00090:'
+                '99900:'),
+          Image('99990:'
+                '90000:'
+                '99990:'
+                '90090:'
+                '99990:')]
+while True:
+  if button_a.was_pressed():
+    for i in range(5):
+      display.show(random.choice(numbers))
+      sleep(750)
+      
